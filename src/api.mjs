4ee80e9eb667e889569f7874572895532cfa1b64
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-01-17 22:47:00 +0700

    Update: apiToken stop calling from API for each call

--- src/api.mjs
+++ src/api.mjs
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+let apiToken = "";
+let expires = 0;
+
 const getAPIToken = async () => {
   const { data: response } = await axios.post(
     "https://api.petfinder.com/v2/oauth2/token",
@@ -10,11 +13,17 @@ const getAPIToken = async () => {
       client_secret: process.env.SECRET_KEY,
     }
   );
+  expires = new Date().getTime() + response.expires_in * 1000;
   return response.access_token;
 };
 
 const fetchPets = async (type, page) => {
-  const apiToken = await getAPIToken();
+  if (apiToken === "") {
+    apiToken = await getAPIToken();
+  } else if (!expires || expires - new Date().getTime() < 1) {
+    apiToken = await getAPIToken();
+  }
+  console.log(apiToken);
   const { data: response } = await axios.get(
     `https://api.petfinder.com/v2/animals?
     ${type ? "type=" + type : ""}

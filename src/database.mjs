62848b6dc87a97120bc84bab9e99df815497e97c
ShinyKiwis <nguyenhua.hcm@gmail.com>
2023-02-11 23:57:33 +0700

    Update: Update a new field avatar for user

--- src/database.mjs
+++ src/database.mjs
@@ -18,6 +18,7 @@ export async function createUser(username, password) {
   const user = {
     username: username,
     password: password,
+    avatar: "",
     bio: "",
     loved: [],
     adopted: [],
